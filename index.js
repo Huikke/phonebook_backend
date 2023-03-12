68ee6363dc67d130f03254edacb245a40b83cea9
Lassi Chen <lachen@utu.fi>
2023-03-12 08:56:37 +0200

    3.4 done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
 
@@ -44,6 +45,13 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
