4ce08b0f20aa74916aff0f8724c1084b1e5b075a
Lassi Chen <lachen@utu.fi>
2023-03-23 22:21:40 +0200

    3.12 and 3.13 done

--- index-nomongo.js
+++ index-nomongo.js
@@ -1,9 +1,30 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const Person = require('./models/person')
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
 
 app.use(express.json())
 app.use(express.static('build'))
@@ -11,27 +32,27 @@ app.use(morgan('tiny'))
 app.use(cors())
   
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+    response.json(persons)
 })
 
-// Doesn't work properly
 app.get('/info', (request, response) => {
     response.send(`
-        <p>Phonebook has info for ${Person.length} people</p>
+        <p>Phonebook has info for ${persons.length} people</p>
         <p>${Date()}</p>
     `)
 })
 
-// Explodes if id doesn't exist
 app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
-    })
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
 })
 
-// Doesn't work
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
@@ -39,12 +60,10 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-// Obsolete
 const generateRandomId = () => {
   return Math.floor(Math.random() * 1000000);
 }
 
-// Has no restrictions for now
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -53,23 +72,23 @@ app.post('/api/persons', (request, response) => {
       error: 'name and/or number missing'
     })
   }
-  /*if (persons.some(person => person.name === body.name)) {
+  if (persons.some(person => person.name === body.name)) {
     return response.status(400).json({
       error: 'name must be unique'
     })
-  } in maintenance*/
+  }
 
-  const person = new Person ({
+  const person = {
     name: body.name,
-    number: body.number
-  })
+    number: body.number,
+    id: generateRandomId()
+  }
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  persons = persons.concat(person)
+  response.json(person)
 })
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
